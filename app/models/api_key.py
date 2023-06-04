db92efe74a724dbe92ad4dc9225551c223bcdf5b
apinanyogaratnam <apinan@ontopical.com>
2023-06-04 19:18:05 -0400

    feat: created api keys db model

---
+++ app/models/api_key.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timezone
+from app import db
+
+from sqlalchemy import false
+from sqlalchemy.dialects.postgresql import TIMESTAMP
+
+
+class ApiKeys(db.Model):
+    __tablename__ = 'api_keys'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, index=True)
+    hashed_api_key = db.Column(db.Text, nullable=False, index=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+    is_deleted = db.Column(db.Boolean, default=False, server_default=false(), nullable=False)
+    created_at = db.Column(TIMESTAMP(timezone=True), default=datetime.now(tz=timezone.utc), server_default=db.func.now(), nullable=False)
+
+    @classmethod
+    def query(cls):
+        return super(ApiKeys, cls).query.filter_by(is_deleted=False)
+    
